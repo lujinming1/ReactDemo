0b994d65aeff288b2407b84a22d1cca6ec966c17
lujinming1 <lujinming163@163.com>
2017-04-13 07:46:53 -0700

    d Please enter the commit message for your changes. Lines starting

--- first-react-demo/src/Demo6.js
+++ first-react-demo/src/Demo6.js
@@ -8,18 +8,21 @@ class Demo6 extends Component {
     };
     this.handleClick = this.handleClick.bind(this);
   }
-handleClick(){
+handleClick(e){
+  var enable = !this.state.enable;
   this.setState({
-    enable:!this.state.enbale
+    enable
   });
-  event.stopPropagation();
-  event.preventDefault();
+  e.preventDefault();
+
 }
 
   render(){
+    var thisState = this.state.enable;
     return (
       <p>
         <input type="text" disabled={this.state.enable}/>
+        <span>{thisState}</span>
         <button onClick={this.handleClick}>点我啊</button>
       </p>
     )
